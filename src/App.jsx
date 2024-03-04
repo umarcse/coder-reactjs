010fd63dade1fb376db0a8084368d8619fc937bd
umarcse <umaarcse@gmail.com>
2024-03-04 11:49:08 +0600

    environment clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+    <div> <h2 className=' text-red-700  text-2xl text-center p-2 border-b-2'>APP ROUTER</h2> </div>
+    <p className='px-2 text-slate-800'> 
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore,
+        unde animi dolore molestiae neque eius assumenda recusandae hic exercitationem nobis minima quae sit molestias blanditiis beatae reiciendis, mollitia, sapiente odio doloribus provident ea. Aliquam error repellendus sit dolores, reiciendis, exercitationem aperiam quis illo velit totam facere corrupti magnam temporibus fugit! Deleniti quis debitis quasi possimus! Quod architecto deserunt commodi ut omnis suscipit nam ullam placeat vitae, accusantium,
+        et quibusdam? Dolorem, doloremque dolores recusandae sapiente aliquid a dicta
+        cumque, modi corporis repellat accusamus aut. Atque illo repellendus iure ducimus, 
+        blanditiis nam laboriosam ipsam doloribus corporis libero unde ex,
+        veniam cupiditate nulla.
+    </p>
+   </>
   )
-}
+}  
 
 export default App
